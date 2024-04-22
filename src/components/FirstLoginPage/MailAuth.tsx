f21852def34fb268218b0274779db970d1058875
masato segawa <masa_14215@docomo.ne.jp>
2024-04-22 20:06:45 +0900

    Add Stepper

--- src/components/FirstLoginPage/MailAuth.tsx
+++ src/components/FirstLoginPage/MailAuth.tsx
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import { NavbarPage, FooterPage } from 'components/MenuItem';
 import Background from 'components/Common/Background';
 import axios from 'axios';
-import { Box, Card, CardContent, Input, TextField } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Input,
+  Step,
+  StepLabel,
+  Stepper,
+  TextField,
+} from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import Button from '@mui/material/Button';
 import styles from './MailAuth.module.scss';
@@ -71,33 +81,59 @@ export const MailAuth: React.VFC = () => {
                 sx={{ flex: { md: '0 0 50%' }, maxWidth: { md: '50%' } }}
               >
                 {/* 追加分 */}
-                <Card>
-                  <CardContent sx={{ padding: '1.00rem' }}>
-                    <p className="h5 text-left mb-4">STEP1:メール登録・認証</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardContent sx={{ padding: '1.25rem' }}>
-                    <p className="h5 text-left mb-4">STEP2</p>
-                    <div id="step2" className={styles.step}>
-                      <h2>Step 2: ユーザ登録</h2>
-                    </div>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardContent>
-                    <div id="step3" className={styles.step}>
-                      <h2>Step 3: 問診チャット</h2>
-                      {/* <input type="text" placeholder="Address">
+                <Stepper alternativeLabel sx={{ marginBottom: '20px' }}>
+                  <Step>
+                    <StepLabel>
+                      <p className="h6 text-center">メール登録・認証</p>
+                    </StepLabel>
+                  </Step>
+                  <Step>
+                    <StepLabel>
+                      <p className="h6 text-center">ユーザー登録</p>
+                    </StepLabel>
+                  </Step>
+                  <Step>
+                    <StepLabel>
+                      <p className="h6 text-center">問診チャット</p>
+                    </StepLabel>
+                  </Step>
+                </Stepper>
+                {/* <Grid container spacing={2}>
+                  <Grid item xs={4}>
+                    <Card>
+                      <CardContent>
+                        <p className="h5 text-left mb-4">
+                          STEP1:メール登録・認証
+                        </p>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Card>
+                      <CardContent>
+                        <p className="h5 text-left mb-4">STEP2</p>
+                        <div id="step2" className={styles.step}>
+                          <h2>Step 2: ユーザ登録</h2>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Card>
+                      <CardContent>
+                        <div id="step3" className={styles.step}>
+                          <h2>Step 3: 問診チャット</h2>
+                          <input type="text" placeholder="Address">
                         <input type="text" placeholder="City">
-                        <input type="text" placeholder="Country"> */}
-                    </div>
-                  </CardContent>
-                </Card>
-
+                        <input type="text" placeholder="Country">
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                </Grid> */}
                 <Card>
                   <CardContent sx={{ padding: '1.25rem' }}>
-                    <p className="h5 text-center mb-4">新規登録</p>
+                    <p className="h5 text-center mb-4">メール登録・認証</p>
                     <form>
                       <div className={styles.input_field}>
                         <div className={styles.input}>
